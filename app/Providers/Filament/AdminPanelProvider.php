a019dce615632b6a83535adc5456d041247b6bb9
Hala Abusalim <82084034+HalaSalim77@users.noreply.github.com>
2025-05-04 22:09:33 +0300

    change logo and publish the config

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,8 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandLogo(asset('images/logo.jpeg'))
+            ->brandLogoHeight('3.5rem')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
