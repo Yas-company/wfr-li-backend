f5d2f4839b58402c274bff911e8ce8b9a6ee0336
Hala Abusalim <82084034+HalaSalim77@users.noreply.github.com>
2025-05-07 16:59:54 +0300

    Enhance Category model by adding HasFactory trait for factory support.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Storage;
 use Spatie\Translatable\HasTranslations;
 
 class Category extends Model
 {
-    use HasTranslations;
+    use HasTranslations , HasFactory;
 
     protected $guarded = [];
 
