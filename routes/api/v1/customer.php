3e4d1f784d50d6ce667c04fa064ab43e44023cde
kamhawy4 <kamhawy4@gmail.com>
2025-07-01 23:29:18 +0300

    done from get data supller by id

--- routes/api/v1/customer.php
+++ routes/api/v1/customer.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\V1\SupplierController;
+use App\Http\Controllers\api\v1\SupplierController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->prefix('supplier')
