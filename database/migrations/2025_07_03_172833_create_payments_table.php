7ac843d43f6639121daa2b99c6f0f788e4d600f8
obada <opadaalziede@gmail.com>
2025-07-05 20:59:51 +0300

    fix: PaymentMethod namespace

--- database/migrations/2025_07_03_172833_create_payments_table.php
+++ database/migrations/2025_07_03_172833_create_payments_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Enums\PaymentMethod;
 use App\Enums\PaymentStatus;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Enums\Order\PaymentMethod;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
