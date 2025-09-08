403aa504663b611364098bee54644ab7f4bbc2bb
obada <opadaalziede@gmail.com>
2025-09-08 13:12:50 +0300

    fix: default product image

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -119,8 +119,8 @@ class Product extends Model implements HasMedia
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('images')
-        ->useFallbackUrl(asset('images/logo.jpeg'))
-        ->useFallbackPath(public_path('images/logo.jpeg'));
+        ->useFallbackUrl(asset('images/product_default.jpeg'))
+        ->useFallbackPath(public_path('images/product_default.jpeg'));
     }
 
     public function favoritedByUsers()
